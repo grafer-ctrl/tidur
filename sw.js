cb8315defc54571d3d3b91d92dff0f743d3f44d5
grafer-ctrl <umarsaid.radiografer@gmail.com>
2025-11-18 14:05:27 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'sleep-calculator-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
