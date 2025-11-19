637d42fe9ae7907303c914ac25d36bb3881741a7
grafer-ctrl <umarsaid.radiografer@gmail.com>
2025-11-19 10:36:32 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-const CACHE = 'sleep-calculator-v1';
-const FILES = [
+const CACHE_NAME = 'sleep-optimizer-v1';
+const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
