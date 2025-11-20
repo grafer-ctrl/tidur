3ae52d41465f7e269b39d2607988eba6287654f4
grafer-ctrl <umarsaid.radiografer@gmail.com>
2025-11-20 21:31:32 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,13 @@
-// Nama cache yang akan digunakan
+// sw.js
 const CACHE_NAME = 'grafer-ctrl-v1';
 
-// Daftar file yang akan di-cache
+// Pastikan '/' dan '/index.html' ada di daftar ini
 const URLS_TO_CACHE = [
-  '/',
-  '/game.html', // Ganti dengan index.html jika itu file utama Anda
+  '/',          // Ini biasanya merujuk ke index.html
+  '/index.html', // Lebih eksplisit untuk menyertakannya
   '/manifest.json',
   '/192.png',
-  // Tambahkan file lainnya seperti CSS, JS, dan ikon lainnya
-  // '/js/main.js',
-  // '/css/style.css'
+  // Tambahkan semua file lain yang dibutuhkan (CSS, JS, gambar, dll)
 ];
 
-// Event install: Mendaftarkan file-file ke cache
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(URLS_TO_CACHE);
-      })
-  );
-});
-
-// Event fetch: Melayani request dari cache jika tersedia
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
+// ... sisanya kode sw.js tetap sama ...
